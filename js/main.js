c62ddd0949f6fe20baab51b05b6f935a0d37a462
minnug <parkminnug@gmail.com>
2024-02-22 17:03:20 +0900

    💄 .whatson 마크업 구조 개선, 스타일 작업 완료(Swiper Slide 추가 등)

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Swiper
+const swiperWhatson= new Swiper('.whatson__img .swiper', {
+  autoplay: true,
+  loop: true,
+  pagination: {
+    el: '.whatson__img .swiper-pagination', 
+    clickable: true 
+  },
+});
