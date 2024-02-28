b7a474ba83b86099ba02bccb11d0f49595f64440
minnug <parkminnug@gmail.com>
2024-02-28 21:26:49 +0900

    ♻️ JS 파일 정리

--- js/main.js
+++ js/main.js
@@ -1,53 +1,138 @@
-const swiperSlide = document.querySelector('.swiper-slide-active');
-const whatsonTitle = document.querySelectorAll('.whatson__txt-title .title');
-const whatsonDate = document.querySelectorAll('.whatson__txt-info .info-date');
-const whatsonPlace = document.querySelectorAll('.whatson__txt-info .info-location');
-const whatonLink = document.querySelectorAll('.whatson__link .link-item');
+const toTopBtn = document.querySelector('.top-btn');
+const searchOpenBtn = document.querySelector('.search .btn-open');
+const searchCloseBtn = document.querySelector('.search .btn-close');
+const searchBox = document.querySelector('.search__open');
+const familyOpenBtn = document.querySelector('.family-btn');
+const familyMenu = document.querySelector('.family-list');
+const headerEl = document.querySelector('.header');
 
-// Swiper
-const swiperWhatson= new Swiper('.whatson__img .swiper', {
-  autoplay: true,
-  loop: true,
-  pagination: {
-    el: '.whatson__img .swiper-pagination', 
-    clickable: true 
-  },
-  on: {
-    init: function () {
-      whatsonTitle[0].classList.add('is-active');
-      whatsonDate[0].style.display = 'block';
-      whatsonPlace[0].style.display = 'block';
-      whatonLink[0].style.display = 'block';
-    },
-    slideChangeTransitionStart:function(){
-      whatsonTitle.forEach(function(element, index){
-        element.classList.remove('is-active');
-        document.querySelectorAll('.whatson__txt-info .info-date')[index].style.display = 'none';
-        document.querySelectorAll('.whatson__txt-info .info-location')[index].style.display = 'none';
-        document.querySelectorAll('.whatson__link .link-item')[index].style.display = 'none';
+// HEADER - 검색 버튼 열기 함수
+function openSearch() {
+  searchBox.classList.add('is-open');
+  gsap.to(searchBox, .3, {
+    opacity: 1,
+    display: 'block'
+  });
+  gsap.to(searchCloseBtn, .3, {
+    scale: 1,
+    display: 'block'
+  });
+  gsap.to(searchOpenBtn, .3, {
+    scale: 0,
+    display: 'none'
+  });
+}
+
+// HEADER - 검색 버튼 닫기 함수
+function closeSearch() {
+  gsap.to(searchBox, .3, {
+    opacity: 0,
+    display: 'none'
+  });
+  searchBox.classList.remove('is-open');
+  gsap.to(searchCloseBtn, .3, {
+    scale: 0,
+    display: 'none'
+  });
+  gsap.to(searchOpenBtn, .3, {
+    scale: 1,
+    display: 'block'
+  });
+}
+
+// HEADER - 검색 버튼 열기/닫기 호출
+searchOpenBtn.addEventListener('click', openSearch);
+searchCloseBtn.addEventListener('click', closeSearch);
+
+
+// HEADER - 위/아래 스크롤 시 show/hide 함수
+let lastScroll = document.documentElement.scrollTop;
+function visibleHeader () {
+  let scrollTop = document.documentElement.scrollTop;
+  if(scrollTop > lastScroll) {
+    // down
+    headerEl.classList.remove('fixed');
+     gsap.to(searchBox, .2, {
+        opacity: 0,
+        visibility: 'hidden'
       });
-      whatsonTitle[swiperWhatson.realIndex].classList.add('is-active');
-      whatsonDate[swiperWhatson.realIndex].style.display = 'block';
-      whatsonPlace[swiperWhatson.realIndex].style.display = 'block';
-      whatonLink[swiperWhatson.realIndex].style.display = 'block';
-    }
+  } else {
+    // up
+    headerEl.classList.add('fixed');
+    gsap.to(searchBox, .2, {
+      opacity: 1,
+      visibility: 'visible'
+    });
   }
+  lastScroll = scrollTop
+}
+
+// FOOTER - 버튼 클릭 시 최상단으로 이동
+toTopBtn.addEventListener('click', function(){
+  gsap.to(window, .5, {
+    scrollTo: 0
+  });
 });
 
-const swiperTicket= new Swiper('.ticket .swiper', {
-  spaceBetween: 74, 
-  loop: true,
-  slidesPerView: 1.7, 
-  centeredSlides: true,
-  pagination: {
-    el: '.ticket .swiper-pagination', 
-    clickable: true 
-  },
-  navigation: {
-    prevEl: '.ticket .swiper-button-prev',
-    nextEl: '.ticket .swiper-button-next'
+// FOOTER - 버튼 클릭 시 최상단으로 이동 함수
+function visibleTopBtn (){
+  if (window.scrollY > 700) {
+    gsap.to(toTopBtn, .3, {
+      opacity: 1,
+      display: 'block'
+    });
+  }  else {
+    gsap.to(toTopBtn, .3, {
+      opacity: 0,
+      display: 'none'
+    });
   }
+}
+
+// 함수 호출
+window.addEventListener('scroll', function() {
+  visibleHeader();
+  visibleTopBtn();
+})
+
+// FOOTER - 패밀리 사이트 열고 닫기
+familyOpenBtn.addEventListener('click', function(){
+  familyMenu.classList.toggle('is-open');
+  this.classList.toggle('is-active');
 });
+
+// FOOTER - 현재 년도 정보 반영
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
+
+// CURSOR
+const cursor = document.querySelector('.cursor');
+const cursorViewLink = document.querySelectorAll('.cursor-view-link');
+const div = document.querySelectorAll('div');
+
+document.addEventListener('mousemove', function(e){
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+div.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('cursor-hover');
+  });
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('cursor-hover');
+  });
+})
+
+cursorViewLink.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('cursor-view');
+    cursor.classList.remove('cursor-hover');
+  });
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('cursor-view');
+    cursor.classList.add('cursor-hover');
+  });
+})
 
 // ScrollMagic
 const spyEls = document.querySelectorAll('.scroll-spy');
---
+++ js/swiper-slide.js
@@ -0,0 +1,49 @@
+const whatsonTitle = document.querySelectorAll('.whatson__txt-title .title');
+const whatsonDate = document.querySelectorAll('.whatson__txt-info .info-date');
+const whatsonPlace = document.querySelectorAll('.whatson__txt-info .info-location');
+const whatonLink = document.querySelectorAll('.whatson__link .link-item');
+
+// Swiper
+const swiperWhatson= new Swiper('.whatson__img .swiper', {
+  autoplay: true,
+  loop: true,
+  pagination: {
+    el: '.whatson__img .swiper-pagination', 
+    clickable: true 
+  },
+  on: {
+    init: function () {
+      whatsonTitle[0].classList.add('is-active');
+      whatsonDate[0].style.display = 'block';
+      whatsonPlace[0].style.display = 'block';
+      whatonLink[0].style.display = 'block';
+    },
+    slideChangeTransitionStart:function(){
+      whatsonTitle.forEach(function(element, index){
+        element.classList.remove('is-active');
+        document.querySelectorAll('.whatson__txt-info .info-date')[index].style.display = 'none';
+        document.querySelectorAll('.whatson__txt-info .info-location')[index].style.display = 'none';
+        document.querySelectorAll('.whatson__link .link-item')[index].style.display = 'none';
+      });
+      whatsonTitle[swiperWhatson.realIndex].classList.add('is-active');
+      whatsonDate[swiperWhatson.realIndex].style.display = 'block';
+      whatsonPlace[swiperWhatson.realIndex].style.display = 'block';
+      whatonLink[swiperWhatson.realIndex].style.display = 'block';
+    }
+  }
+});
+
+const swiperTicket= new Swiper('.ticket .swiper', {
+  spaceBetween: 74, 
+  loop: true,
+  slidesPerView: 1.7, 
+  centeredSlides: true,
+  pagination: {
+    el: '.ticket .swiper-pagination', 
+    clickable: true 
+  },
+  navigation: {
+    prevEl: '.ticket .swiper-button-prev',
+    nextEl: '.ticket .swiper-button-next'
+  }
+});
